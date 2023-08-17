6c8649e4a49172c40e640785a75dd06a9deca252
Evgeny Novoselov <enovoselov@yahoo.com>
2023-08-17 15:13:45 +0300

    Добавил модель Task

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,23 @@
+package ru.job4j.todo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String description;
+    private LocalDateTime created;
+    private Boolean done;
+}
