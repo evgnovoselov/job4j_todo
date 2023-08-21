dd8c27d8573fcdae890c2ba72206722fd50961fa
Evgeny Novoselov <enovoselov@yahoo.com>
2023-08-21 13:14:55 +0300

    Добавил контроллер и шаблон задач

--- src/main/java/ru/job4j/todo/App.java
+++ src/main/java/ru/job4j/todo/App.java
@@ -1,10 +1,11 @@
 package ru.job4j.todo;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -0,0 +1,17 @@
+package ru.job4j.todo.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@AllArgsConstructor
+public class TaskController {
+
+    @GetMapping()
+    public String getAll() {
+        return "tasks/list";
+    }
+}
