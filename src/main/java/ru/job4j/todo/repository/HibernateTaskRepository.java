4fb7e200fdb672cc0f49b56e46fe55de537071fd
Evgeny Novoselov <enovoselov@yahoo.com>
2023-08-27 15:53:44 +0300

    Исправил работу HibernateTaskRepository

--- src/main/java/ru/job4j/todo/repository/HibernateTaskRepository.java
+++ src/main/java/ru/job4j/todo/repository/HibernateTaskRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -20,7 +21,7 @@ public class HibernateTaskRepository implements TaskRepository {
         return fromTransaction(session -> {
             session.persist(task);
             return true;
-        }) != null;
+        }).orElse(false);
     }
 
     @Override
@@ -28,27 +29,33 @@ public class HibernateTaskRepository implements TaskRepository {
         return fromTransaction(session -> {
             session.merge(task);
             return true;
-        }) != null;
+        }).orElse(false);
     }
 
     @Override
     public Collection<Task> findAllByOrderByCreatedDesc() {
-        return fromTransaction(session -> session.createQuery("from Task order by created desc", Task.class).list());
+        return fromTransaction(session -> session
+                .createQuery("from Task order by created desc", Task.class)
+                .list()).orElse(Collections.emptyList());
     }
 
     @Override
     public Collection<Task> findAllByDoneTrueOrderByCreatedDesc() {
-        return fromTransaction(session -> session.createQuery("from Task where done = true order by created desc", Task.class).list());
+        return fromTransaction(session -> session
+                .createQuery("from Task where done = true order by created desc", Task.class)
+                .list()).orElse(Collections.emptyList());
     }
 
     @Override
     public Collection<Task> findAllByDoneFalseOrderByCreatedDesc() {
-        return fromTransaction(session -> session.createQuery("from Task where done = false order by created desc", Task.class).list());
+        return fromTransaction(session -> session
+                .createQuery("from Task where done = false order by created desc", Task.class)
+                .list()).orElse(Collections.emptyList());
     }
 
     @Override
     public Optional<Task> findById(int id) {
-        return Optional.ofNullable(fromTransaction(session -> session.get(Task.class, id)));
+        return fromTransaction(session -> session.get(Task.class, id));
     }
 
     @Override
@@ -58,26 +65,26 @@ public class HibernateTaskRepository implements TaskRepository {
             task.setId(id);
             session.remove(task);
             return true;
-        }) != null;
+        }).orElse(false);
     }
 
     @Override
     public boolean setStatusById(int id, boolean done) {
         return fromTransaction(session -> {
-            session.createMutationQuery("update Task set done = :done where id = :id")
+            int changed = session.createMutationQuery("update Task set done = :done where id = :id")
                     .setParameter("done", done)
                     .setParameter("id", id)
                     .executeUpdate();
-            return true;
-        }) != null;
+            return changed > 0;
+        }).orElse(false);
     }
 
-    private <R> R fromTransaction(Function<Session, R> action) {
-        R result = null;
+    private <R> Optional<R> fromTransaction(Function<Session, R> action) {
+        Optional<R> result = Optional.empty();
         Session session = sf.openSession();
         try (session) {
             session.beginTransaction();
-            R apply = action.apply(session);
+            Optional<R> apply = Optional.ofNullable(action.apply(session));
             session.getTransaction().commit();
             result = apply;
         } catch (Exception e) {
