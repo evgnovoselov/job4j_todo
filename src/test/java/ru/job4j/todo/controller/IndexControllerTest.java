546ac11e1855c5e5c8b456bd9c52aaf1fbb41758
Evgeny Novoselov <enovoselov@yahoo.com>
2023-08-27 20:47:09 +0300

    Добавил тест IndexControllerTest

---
+++ src/test/java/ru/job4j/todo/controller/IndexControllerTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.todo.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IndexControllerTest {
+    private IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void whenGetIndexPageThenRedirectToTasks() {
+        String view = indexController.index();
+
+        Assertions.assertThat(view).isEqualTo("redirect:/tasks");
+    }
+}
