4db8cd6933e4200efb9483b2c1f2f0afa9d3329e
Evgeny Novoselov <enovoselov@yahoo.com>
2023-09-08 20:29:59 +0300

    Добавил регистрацию и тест

--- src/main/java/ru/job4j/todo/controller/UserController.java
+++ src/main/java/ru/job4j/todo/controller/UserController.java
@@ -1,19 +1,36 @@
 package ru.job4j.todo.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.todo.model.User;
+import ru.job4j.todo.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     @GetMapping("/registration")
     public String getRegistrationPage(Model model) {
         model.addAttribute("user", new User());
         return "users/registration";
+    }
+
+    @PostMapping("/registration")
+    public String processRegistration(User user, Model model, HttpSession session) {
+        boolean isSave = userService.save(user);
+        if (!isSave) {
+            model.addAttribute("error", "Пользователь с таким логином уже существует.");
+            model.addAttribute("user", user);
+            return "users/registration";
+        }
+        session.setAttribute("user", user);
+        return "redirect:/";
     }
 }
--- src/test/java/ru/job4j/todo/controller/UserControllerTest.java
+++ src/test/java/ru/job4j/todo/controller/UserControllerTest.java
@@ -2,17 +2,23 @@ package ru.job4j.todo.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.ui.ConcurrentModel;
 import ru.job4j.todo.model.User;
+import ru.job4j.todo.service.UserService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static ru.job4j.todo.util.UserUtil.makeUser;
 
 class UserControllerTest {
     private UserController userController;
+    private UserService userService;
 
     @BeforeEach
     void setUp() {
-        userController = new UserController();
+        userService = mock(UserService.class);
+        userController = new UserController(userService);
     }
 
     @Test
@@ -23,5 +29,35 @@ class UserControllerTest {
 
         assertThat(view).isEqualTo("users/registration");
         assertThat(actualUser).usingRecursiveComparison().isEqualTo(new User());
+    }
+
+    @Test
+    void whenRegistrationUserSuccessThenRedirectIndexPageWithUserInSession() {
+        User user = makeUser(7);
+        when(userService.save(any(User.class))).thenReturn(true);
+        ConcurrentModel model = new ConcurrentModel();
+        MockHttpSession session = new MockHttpSession();
+
+        String view = userController.processRegistration(user, model, session);
+        User actualUserSession = (User) session.getAttribute("user");
+
+        assertThat(view).isEqualTo("redirect:/");
+        assertThat(actualUserSession).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    void whenRegistrationUserNotSaveThenGetPageRegistrationUserWithMessage() {
+        User user = makeUser(7);
+        when(userService.save(any(User.class))).thenReturn(false);
+        ConcurrentModel model = new ConcurrentModel();
+        MockHttpSession session = new MockHttpSession();
+
+        String view = userController.processRegistration(user, model, session);
+        String error = (String) model.getAttribute("error");
+        User actualUser = (User) model.getAttribute("user");
+
+        assertThat(view).isEqualTo("users/registration");
+        assertThat(error).isEqualTo("Пользователь с таким логином уже существует.");
+        assertThat(actualUser).usingRecursiveComparison().isEqualTo(user);
     }
 }
