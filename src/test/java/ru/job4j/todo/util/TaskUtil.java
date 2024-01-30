55d5ce2deb043e86f70f42afa4bb372a5655ab84
Evgeny Novoselov <enovoselov@yahoo.com>
2024-01-30 20:29:52 +0300

    Поправил TaskUtil

--- src/test/java/ru/job4j/todo/util/TaskUtil.java
+++ src/test/java/ru/job4j/todo/util/TaskUtil.java
@@ -1,8 +1,13 @@
 package ru.job4j.todo.util;
 
+import ru.job4j.todo.model.Category;
+import ru.job4j.todo.model.Priority;
 import ru.job4j.todo.model.Task;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class TaskUtil {
     public static Task makeTask(int seed, boolean done) {
@@ -13,6 +18,18 @@ public final class TaskUtil {
         task.setDescription("Task description " + seed);
         task.setCreated(dateTime);
         task.setDone(done);
+        Set<Category> categories = IntStream.of(1, 2).mapToObj(i -> {
+            Category category = new Category();
+            category.setId(i);
+            category.setName("Category " + i);
+            return category;
+        }).collect(Collectors.toSet());
+        task.setCategories(categories);
+        Priority priority = new Priority();
+        priority.setId(1);
+        priority.setName("Priority 1");
+        priority.setPosition(1);
+        task.setPriority(priority);
         return task;
     }
 }
