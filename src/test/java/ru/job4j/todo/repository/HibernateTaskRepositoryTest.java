93aab68194b1ec880233a120fc5c263945d4aeae
Evgeny Novoselov <enovoselov@yahoo.com>
2024-02-09 16:49:23 +0300

    Поправил тесты HibernateTaskRepositoryTest

--- src/test/java/ru/job4j/todo/repository/HibernateTaskRepositoryTest.java
+++ src/test/java/ru/job4j/todo/repository/HibernateTaskRepositoryTest.java
@@ -7,10 +7,7 @@ import ru.job4j.todo.model.Task;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -18,8 +15,6 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static ru.job4j.todo.util.TaskUtil.makeTask;
 
-// TODO fix
-@Disabled("Need fix")
 class HibernateTaskRepositoryTest {
     private static SessionFactory sessionFactory;
     private static HibernateTaskRepository taskRepository;
@@ -40,35 +35,31 @@ class HibernateTaskRepositoryTest {
         sessionFactory.close();
     }
 
-    // TODO fix
     @Test
-    @Disabled
-    void whenSaveTaskThenReturnTrueAndSetIdInTask() {
-//        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-//        Task task = new Task();
-//        task.setTitle("title");
-//        task.setDescription("description");
-//        task.setCreated(now);
-//
-//        boolean isSave = taskRepository.save(task);
-//        Task actualTask = taskRepository.findById(task.getId()).orElseThrow();
-//
-//        assertThat(isSave).isTrue();
-//        assertThat(actualTask).usingRecursiveComparison().isEqualTo(task);
+    void whenSaveTaskThenReturnIdAndSetIdInTask() {
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        Task task = new Task();
+        task.setTitle("title");
+        task.setDescription("description");
+        task.setCreated(now);
+
+        Optional<Integer> savedId = taskRepository.save(task);
+        Task actualTask = taskRepository.findById(task.getId()).orElseThrow();
+
+        assertThat(savedId.isPresent()).isTrue();
+        assertThat(actualTask).usingRecursiveComparison().isEqualTo(task);
     }
 
-    // TODO fix
-    @Disabled
     @Test
     void whenSaveTaskAndCrudRepThrowRuntimeExceptionThenReturnFalse() {
-//        CrudRepository crudRepository = mock(CrudRepository.class);
-//        HibernateTaskRepository taskRepositoryMock = new HibernateTaskRepository(crudRepository);
-//        doThrow(RuntimeException.class).when(crudRepository).run(any());
-//
-//        Task task = new Task();
-//        boolean isSave = taskRepositoryMock.save(task);
-//
-//        assertThat(isSave).isFalse();
+        CrudRepository crudRepository = mock(CrudRepository.class);
+        HibernateTaskRepository taskRepositoryMock = new HibernateTaskRepository(crudRepository);
+        doThrow(RuntimeException.class).when(crudRepository).run(any());
+
+        Task task = new Task();
+        Optional<Integer> savedId = taskRepositoryMock.save(task);
+
+        assertThat(savedId.isPresent()).isFalse();
     }
 
     @Test
@@ -86,6 +77,8 @@ class HibernateTaskRepositoryTest {
     void whenUpdateTaskCorrectThenReturnTrueAndTaskUpdated() {
         Task task = makeTask(7, false);
         task.setId(null);
+        task.setPriority(null);
+        task.setCategories(null);
         taskRepository.save(task);
 
         Task updateTask = new Task();
@@ -117,25 +110,25 @@ class HibernateTaskRepositoryTest {
         assertThat(actualTask).isEmpty();
     }
 
-    // TODO fix
-    @Disabled
     @Test
     void whenFindAllByOrderByCreatedDescThenGetTasksOrderedDesc() {
-//        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-//        List<Task> tasks = List.of(
-//                makeTask(9, false),
-//                makeTask(8, false),
-//                makeTask(7, false)
-//        );
-//        tasks.forEach(task -> {
-//            task.setCreated(now.plusHours(task.getId()));
-//            task.setId(null);
-//        });
-//        tasks.forEach(taskRepository::save);
-//
-//        List<Task> actualTasks = (List<Task>) taskRepository.findAllByOrderByCreatedDesc();
-//
-//        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(tasks);
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        List<Task> tasks = List.of(
+                makeTask(9, false),
+                makeTask(8, false),
+                makeTask(7, false)
+        );
+        tasks.forEach(task -> {
+            task.setCreated(now.plusHours(task.getId()));
+            task.setPriority(null);
+            task.setCategories(Set.of());
+            task.setId(null);
+        });
+        tasks.forEach(taskRepository::save);
+
+        List<Task> actualTasks = (List<Task>) taskRepository.findAllByOrderByCreatedDesc();
+
+        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(tasks);
     }
 
     @Test
@@ -149,28 +142,28 @@ class HibernateTaskRepositoryTest {
         assertThat(tasks).isEmpty();
     }
 
-    // TODO fix
-    @Disabled
     @Test
     void whenFindAllByDoneTrueOrderByCreatedDescThenReturnTasksDoneAndCreatedDesc() {
-//        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-//        List<Task> tasks = List.of(
-//                makeTask(9, true),
-//                makeTask(8, false),
-//                makeTask(7, true),
-//                makeTask(6, false),
-//                makeTask(5, true)
-//        );
-//        tasks.forEach(task -> {
-//            task.setCreated(now.plusHours(task.getId()));
-//            task.setId(null);
-//        });
-//        tasks.forEach(taskRepository::save);
-//
-//        List<Task> actualTasks = (List<Task>) taskRepository.findAllByDoneOrderByCreatedDesc(true);
-//        List<Task> expectedTasks = tasks.stream().filter(task -> Objects.equals(task.isDone(), true)).toList();
-//
-//        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(expectedTasks);
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        List<Task> tasks = List.of(
+                makeTask(9, true),
+                makeTask(8, false),
+                makeTask(7, true),
+                makeTask(6, false),
+                makeTask(5, true)
+        );
+        tasks.forEach(task -> {
+            task.setCreated(now.plusHours(task.getId()));
+            task.setPriority(null);
+            task.setCategories(Set.of());
+            task.setId(null);
+        });
+        tasks.forEach(taskRepository::save);
+
+        List<Task> actualTasks = (List<Task>) taskRepository.findAllByDoneOrderByCreatedDesc(true);
+        List<Task> expectedTasks = tasks.stream().filter(task -> Objects.equals(task.isDone(), true)).toList();
+
+        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(expectedTasks);
     }
 
     @Test
@@ -184,34 +177,36 @@ class HibernateTaskRepositoryTest {
         assertThat(tasks).isEmpty();
     }
 
-    // TODO fix
-    @Disabled
     @Test
     void whenFindAllByDoneFalseOrderByCreatedDescThenReturnTasksNotDoneAndCreatedDesc() {
-//        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-//        List<Task> tasks = List.of(
-//                makeTask(9, true),
-//                makeTask(8, false),
-//                makeTask(7, true),
-//                makeTask(6, false),
-//                makeTask(5, true)
-//        );
-//        tasks.forEach(task -> {
-//            task.setCreated(now.plusHours(task.getId()));
-//            task.setId(null);
-//        });
-//        tasks.forEach(taskRepository::save);
-//
-//        List<Task> actualTasks = (List<Task>) taskRepository.findAllByDoneOrderByCreatedDesc(false);
-//        List<Task> expectedTasks = tasks.stream().filter(task -> Objects.equals(task.isDone(), false)).toList();
-//
-//        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(expectedTasks);
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        List<Task> tasks = List.of(
+                makeTask(9, true),
+                makeTask(8, false),
+                makeTask(7, true),
+                makeTask(6, false),
+                makeTask(5, true)
+        );
+        tasks.forEach(task -> {
+            task.setCreated(now.plusHours(task.getId()));
+            task.setPriority(null);
+            task.setCategories(Set.of());
+            task.setId(null);
+        });
+        tasks.forEach(taskRepository::save);
+
+        List<Task> actualTasks = (List<Task>) taskRepository.findAllByDoneOrderByCreatedDesc(false);
+        List<Task> expectedTasks = tasks.stream().filter(task -> Objects.equals(task.isDone(), false)).toList();
+
+        assertThat(actualTasks).usingRecursiveComparison().isEqualTo(expectedTasks);
     }
 
     @Test
     void whenSetStatusByIdCorrectThenReturnTrueAndChangeStatus() {
         Task task = makeTask(7, false);
         task.setId(null);
+        task.setPriority(null);
+        task.setCategories(Set.of());
         taskRepository.save(task);
 
         boolean hasChange = taskRepository.setStatusById(task.getId(), true);
@@ -243,6 +238,8 @@ class HibernateTaskRepositoryTest {
     void whenDeleteByIdCorrectThenReturnTrue() {
         Task task = makeTask(7, false);
         task.setId(null);
+        task.setPriority(null);
+        task.setCategories(Set.of());
         taskRepository.save(task);
 
         boolean isDelete = taskRepository.deleteById(task.getId());
