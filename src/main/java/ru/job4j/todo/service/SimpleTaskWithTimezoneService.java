d8de0a3c2c875484149a2782c8938602fab8bc79
Evgeny Novoselov <enovoselov@yahoo.com>
2024-03-28 17:02:02 +0300

    Написал работу сервиса с таймзоной

--- src/main/java/ru/job4j/todo/service/SimpleTaskWithTimezoneService.java
+++ src/main/java/ru/job4j/todo/service/SimpleTaskWithTimezoneService.java
@@ -3,6 +3,8 @@ package ru.job4j.todo.service;
 import org.springframework.stereotype.Service;
 import ru.job4j.todo.dto.TaskUpdateDto;
 import ru.job4j.todo.mapper.TaskMapper;
+import ru.job4j.todo.model.Category;
+import ru.job4j.todo.model.Priority;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.repository.CategoryRepository;
 import ru.job4j.todo.repository.PriorityRepository;
@@ -14,10 +16,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SimpleTaskWithTimezoneService extends SimpleTaskService implements TaskWithTimezoneService {
     private static final Set<String> TIMEZONE_IDS = Collections.unmodifiableSet(TimeZoneUtil.getTimeZoneIds());
+    private final TaskMapper taskMapper;
 
     public SimpleTaskWithTimezoneService(
             TaskRepository taskRepository,
@@ -26,11 +30,13 @@ public class SimpleTaskWithTimezoneService extends SimpleTaskService implements 
             TaskMapper taskMapper
     ) {
         super(taskRepository, categoryRepository, priorityRepository, taskMapper);
+        this.taskMapper = taskMapper;
     }
 
     @Override
     public Collection<Task> findAllByOrderByCreatedDesc(String timezoneId) {
         Collection<Task> tasks = findAllByOrderByCreatedDesc();
+        // TODO: 22.03.2024 Duplicate code
         if (TIMEZONE_IDS.contains(timezoneId)) {
             tasks = tasks.stream()
                     .peek(task -> task.setCreated(
@@ -46,16 +52,65 @@ public class SimpleTaskWithTimezoneService extends SimpleTaskService implements 
 
     @Override
     public Collection<Task> findAllByDoneOrderByCreatedDesc(boolean done, String timezoneId) {
-        return null;
+        Collection<Task> tasks = findAllByDoneOrderByCreatedDesc(done);
+        // TODO: 22.03.2024 Duplicate code
+        if (TIMEZONE_IDS.contains(timezoneId)) {
+            tasks = tasks.stream()
+                    .peek(task -> task.setCreated(
+                            task.getCreated()
+                                    .atZone(ZoneId.of("UTC"))
+                                    .withZoneSameInstant(ZoneId.of(timezoneId))
+                                    .toLocalDateTime()
+                    ))
+                    .toList();
+        }
+        return tasks;
     }
 
     @Override
     public Optional<Task> findById(int id, String timezoneId) {
-        return Optional.empty();
+        Optional<Task> taskOptional = findById(id);
+        // TODO: 22.03.2024 Duplicate code
+        if (TIMEZONE_IDS.contains(timezoneId)) {
+            taskOptional.ifPresent(task -> task.setCreated(
+                    task.getCreated()
+                            .atZone(ZoneId.of("UTC"))
+                            .withZoneSameInstant(ZoneId.of(timezoneId))
+                            .toLocalDateTime()
+            ));
+        }
+        return taskOptional;
     }
 
     @Override
     public Optional<TaskUpdateDto> getTaskUpdateDtoById(int id, String timezoneId) {
-        return Optional.empty();
+        Optional<TaskUpdateDto> taskUpdateDtoOptional = getTaskUpdateDtoById(id);
+        if (taskUpdateDtoOptional.isEmpty()) {
+            return Optional.empty();
+        }
+        // TODO: 22.03.2024 Duplicate code
+        if (TIMEZONE_IDS.contains(timezoneId)) {
+            Task task = taskMapper.toTask(
+                    taskUpdateDtoOptional.get(),
+                    taskUpdateDtoOptional.get().categoryIds().stream().map(categoryId -> {
+                        Category category = new Category();
+                        category.setId(categoryId);
+                        return category;
+                    }).collect(Collectors.toSet()),
+                    taskUpdateDtoOptional.map(taskUpdateDto -> {
+                        Priority priority = new Priority();
+                        priority.setId(taskUpdateDto.priorityId());
+                        return priority;
+                    }).get()
+            );
+            task.setCreated(
+                    task.getCreated()
+                            .atZone(ZoneId.of("UTC"))
+                            .withZoneSameInstant(ZoneId.of(timezoneId))
+                            .toLocalDateTime()
+            );
+            taskUpdateDtoOptional = Optional.of(taskMapper.toTaskUpdateDto(task));
+        }
+        return taskUpdateDtoOptional;
     }
 }
